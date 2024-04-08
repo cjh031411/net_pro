e6dca95665374c6077150ddce888dc5cc8ad5b57
Jihun Cheon <cheonjihun0314@naver.com>
2024-04-09 03:55:14 +0900

    9

--- hello.py
+++ hello.py
@@ -1,30 +1,27 @@
-days = {'January':31, 'February':28, 'March':31, 'April':30,
-        'May':31, 'June':30, 'July':31, 'August':31,
-        'September':30, 'October':31, 'November':30,
-        'December':31}
+d= [{'name':'Todd', 'phone':'555-1414', 'email':'todd@mail.net'},
+    {'name':'Helga', 'phone':'555-1618', 'email':'helga@mail.net'},
+    {'name':'Princess', 'phone':'555-3141', 'email':''},
+    {'name':'LJ', 'phone':'555-2718', 'email':'lj@mail.net'}]
 
-#사용자가 월을 입력하면 해당 월에 일수를 출력하라
-month=input()
-print(days[month])
+#전화번호가 8로 끝나는 사용자 이름을 출력하라
+for i in d:
+    if i['phone'][-1]=='8':
+        print(i['name'],end=' ')
+print()
 
-#알파벳 순서로 모든 월을 출력하라.
-month_list=list(days.keys())
-month_list.sort()
-print(month_list)
+#이메일이 없는 사용자 이름을 출력하라
+for i in d:
+    if i['email']=='':
+        print(i['name'],end=' ')
+print()
 
-#일수가 31인 월을 모두 출력하라
-day31_list=[]
-for i in days.keys():
-    if days[i]==31:
-        day31_list.append(i)
-print(day31_list)
-
-#월의 일수를 기준으로 오름차순으로 (key-value) 쌍을 출력하라
-sorted_days=sorted(days.items(),key=lambda item:item[1])
-print(sorted_days)
-
-#사용자가 월을 3자리만 입력하면 월의 일수를 출력하라.(Jan, Feb 등)
-word=input()
-for i in days.keys():
-    if word in i:
-        print(days[i])
+#사용자 이름을 입력하면 전화번호, 이메일을 출력하라. 이름이 없으면 '이름이 없습니다'라는 메시지를 출력하라
+inp_name=input()
+    
+found=False
+for i in d:
+    if inp_name==i['name']:
+        print(i['phone'],i['email'])
+        found=True
+if found==False:
+    print("이름이 없습니다")
