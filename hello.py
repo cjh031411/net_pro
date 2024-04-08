d4f6e988e474bef933eb9fa99d14456e208d51be
Jihun Cheon <cheonjihun0314@naver.com>
2024-04-09 02:26:23 +0900

    1

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello world")
+friends=["kim","lee","park"]
+print(friends)
+friends.insert(0,"choi")
+print(friends)
+friends.insert(2,"sun")
+print(friends)
+friends.append("jang")
+print(friends)
