c79bfda351d392c58939f121e77f18b20ecad359
Jihun Cheon <cheonjihun0314@naver.com>
2024-04-09 14:48:56 +0900

    10

--- hello.py
+++ hello.py
@@ -1,27 +1,20 @@
-d= [{'name':'Todd', 'phone':'555-1414', 'email':'todd@mail.net'},
-    {'name':'Helga', 'phone':'555-1618', 'email':'helga@mail.net'},
-    {'name':'Princess', 'phone':'555-3141', 'email':''},
-    {'name':'LJ', 'phone':'555-2718', 'email':'lj@mail.net'}]
+word='led=on&motor=off&switch=off'
 
-#전화번호가 8로 끝나는 사용자 이름을 출력하라
-for i in d:
-    if i['phone'][-1]=='8':
-        print(i['name'],end=' ')
-print()
+d=dict()
+tmp=str()
+l=[]
 
-#이메일이 없는 사용자 이름을 출력하라
-for i in d:
-    if i['email']=='':
-        print(i['name'],end=' ')
-print()
-
-#사용자 이름을 입력하면 전화번호, 이메일을 출력하라. 이름이 없으면 '이름이 없습니다'라는 메시지를 출력하라
-inp_name=input()
+for i in word:
+    if i=='=' or i=='&':
+        l.append(tmp)
+        tmp=''
+    else:
+        tmp+=i
     
-found=False
-for i in d:
-    if inp_name==i['name']:
-        print(i['phone'],i['email'])
-        found=True
-if found==False:
-    print("이름이 없습니다")
+l.append(tmp)
+print(l)
+
+for i in range(len(l)//2):
+    d[l[i*2]]=l[i*2+1]
+
+print(d)
